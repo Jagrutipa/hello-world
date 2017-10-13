8d8c03da523b52f03d422f56f546e3de89146cb7
Jagrutipa <31985166+Jagrutipa@users.noreply.github.com>
2017-10-13 16:05:13 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,1 +1,14 @@
 
+l=[]
+c=0
+while c<10:
+    n=int(input("enter no.:"))
+    l.append(n)
+    c=c+1
+    for i in l:
+        if i%2==0:
+            print("is even")
+        else:
+            print("is odd")
+            
+                
